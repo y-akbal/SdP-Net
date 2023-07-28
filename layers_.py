0614663650e177f2f72168e98f0269c198979239
Yıldırım Akbal <yildirim.akbal@gmail.com>
2023-07-29 01:25:54 +0300

    Add files via upload

--- layers_.py
+++ layers_.py
@@ -7,12 +7,14 @@ import torch.nn.functional as F
 
 x = torch.rand(10, 3, 224, 224)
 
+nn.Conv2d(3, 3, kernel_size = 4, groups = 3,  padding = "same")(x).shape
+
+
+
 l = torch.rand(3, 23)
 
 Q = torch.einsum("ij,bilk->bjlk", l, x) 
 T = (x.transpose(-3, -1) @ l).transpose(-3, -1)
-
-
 
 
 
@@ -23,21 +25,27 @@ class MultiHeadAtttention(
         super().__init__()
         ## Some simple dimension stuff
         assert (C_in/heads).is_integer(),  "Waccha divisibility criterion buddy"
-        self.embedding_dim = C_in
         self.heads = heads
 
         ### Dropout along channel dimension
         self.dropout_Q = nn.Dropout2d(p=dropout)
         self.dropout_K = nn.Dropout2d(p=dropout)
         self.dropout_V = nn.Dropout2d(p=dropout)
-
+        ### Pick up attention parameters, 
         if the_same_QKV_matrix:
-            self.W = nn.Parameter(
+            W = nn.Parameter(
             torch.randn(C_in, C_in) * (C_in) ** (-0.5)
         )
-        
+            self.L_Q, self.L_K, self.L_V = W, W, W
+        else:
+            pass
+            
         ### This is for mixin' the output channels after attention
         self.output_dense = nn.Parameter(torch.randn(C_in, C_in) * (C_in) ** (-0.5))
+        # Use here flash attention ---- ####
         
+
+    def forward(self, x):
         
-            
+                
+        return x
