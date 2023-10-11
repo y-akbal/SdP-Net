ccae86acc378793c57f2a7d3792b3a6b4354fb7d
Yıldırım Akbal <yildirim.akbal@gmail.com>
2023-10-11 23:20:09 +0300

    layers started

--- layers_.py
+++ layers_.py
@@ -8,48 +8,23 @@ import torch.nn.functional as F
 ## We shall mostly use the optimized torch layers
 ## rather than coming up with our own implementations
 
-
-x = torch.rand(10, 3, 224, 224)
-
-nn.Conv2d(3, 3, kernel_size = 4, groups = 3,  padding = "same")(x).shape
-
-
-
-l = torch.rand(3, 25)
-
-Q = torch.einsum("ij,bilk->bjlk", l, x) 
-T = (x.transpose(-3, -1) @ l).transpose(-3, -1)
-
-
-
-class MultiHeadAtttention(
-    nn.Module
-):  
-    def __init__(self, C_in=128, heads=4, dropout=0.2, the_same_QKV_matrix = True):
+class conv_int(nn.Module):
+    def __init__(self, embedding_dim = 100, patch_size = 4, activation = nn.GELU()):
         super().__init__()
-        ## Some simple dimension stuff
-        assert (C_in/heads).is_integer(),  "Waccha divisibility criterion buddy"
-        self.heads = heads
-
-        ### Dropout along channel dimension
-        self.dropout_Q = nn.Dropout2d(p=dropout)
-        self.dropout_K = nn.Dropout2d(p=dropout)
-        self.dropout_V = nn.Dropout2d(p=dropout)
-        ### Pick up attention parameters, 
-        if the_same_QKV_matrix:
-            W = nn.Parameter(
-            torch.randn(C_in, C_in) * (C_in) ** (-0.5)
-        )
-            self.L_Q, self.L_K, self.L_V = W, W, W
-        else:
-            pass
-            
-        ### This is for mixin' the output channels after attention
-        self.output_dense = nn.Parameter(torch.randn(C_in, C_in) * (C_in) ** (-0.5))
-        # Use here flash attention ---- ####
-        
-
+        self.embedding_dim = embedding_dim
+        self.activation = activation
+        self.conv = nn.Conv2d(in_channels = 3, 
+                              out_channels = embedding_dim,
+                              kernel_size = patch_size,
+                              stride = patch_size
+                              )
     def forward(self, x):
+        x = self.conv(x)
+        return self.activation(x) 
+
+class mixer_conv(nn.Module):
+    def __init__(self):
         
-                
-        return x
+        ## depthwise
+        ## 
+
