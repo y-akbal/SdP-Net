8b49bb0876c554e1974048a5d88640d4ac09fb06
Yıldırım Akbal <yildirim.akbal@gmail.com>
2023-10-12 15:13:31 +0300

    layers_added

--- layers_.py
+++ layers_.py
@@ -74,3 +74,37 @@ class squeezer(nn.Module):
 """
 squeezer(100)(torch.randn(1, 100, 224,224)).shape
 """    
+class encoder_layer(nn.Module):
+    def __init__(self, 
+                 embedding_dim, 
+                 n_head,
+                 activation_func = nn.GELU(),
+                 ):
+        super().__init__()
+        self.transformer_layer = nn.TransformerEncoderLayer(
+            d_model = embedding_dim,
+            nhead = n_head,
+            activation = activation_func,
+            batch_first = True,
+            dim_feedforward = 512,
+        )
+    def forward(self, x):
+        batch_size, C, H, W = x.shape
+        x = x.view(batch_size, C, H*W)
+        x = self.transformer_layer(x)
+        return x
+
+"""
+encoder_layer(100, 5)(torch.randn(1, 100, 10,10))
+
+x = torch.randn(1, 100, 100)
+nn.TransformerEncoderLayer(100,100)()
+encoder_layer = nn.TransformerEncoderLayer(d_model=512, 
+                                           nhead=8, 
+                                           batch_first = True,
+                                           activation = nn.GELU()
+                                           )
+src = torch.rand(1, 5, 512)
+encoder_layer(src).std(2)
+torch.randn(1, 10, 23, 23)
+"""
