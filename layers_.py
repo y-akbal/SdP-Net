b81d2f5f1f2c39c03f9cf373a3948482b327f4c5
Yıldırım Akbal <yildirim.akbal@gmail.com>
2023-10-12 14:39:17 +0300

    new layers added

--- layers_.py
+++ layers_.py
@@ -26,13 +26,14 @@ class conv_int(nn.Module):
 
 
 class conv_mixer(nn.Module):
-    def __init__(self, embedding_dim, 
+    def __init__(self, 
+                 embedding_dim, 
                  kernel_size = 5, 
                  activation = nn.GELU()):
         super().__init__()
         self.conv2d = nn.Conv2d(in_channels = embedding_dim, 
                               out_channels = embedding_dim,
-                              kernel_size = 5,
+                              kernel_size = kernel_size,
                               groups = embedding_dim,
                               padding = "same"
                               )
@@ -44,9 +45,9 @@ class conv_mixer(nn.Module):
         self.batch_norm_1 = nn.BatchNorm2d(embedding_dim)
         self.batch_norm_2 = nn.BatchNorm2d(embedding_dim)
         self.activation = activation
-    def forward(self, x):
-        x_ = self.conv2d(x)
-        x = self.activation(x_)
+    def forward(self, x_):
+        x = self.conv2d(x_)
+        x = self.activation(x)
         x = self.batch_norm_1(x)
         x += x_
         x = self.conv1d(x)
@@ -54,4 +55,22 @@ class conv_mixer(nn.Module):
         x = self.batch_norm_2(x)
         return x
 
-conv_mixer(10)(torch.randn(1, 10, 224,224)).shape
+"""
+conv_mixer(200)(torch.randn(10, 200, 224,224)).std(1)
+"""
+
+class squeezer(nn.Module):
+    def __init__(self, embedding_dim,
+                 squeeze_ratio = 5
+                 ):
+        super().__init__()
+        self.conv = nn.Conv2d(embedding_dim, 
+                              embedding_dim, 
+                              kernel_size = squeeze_ratio,
+                              stride = squeeze_ratio)
+    def forward(self, x):
+        return self.conv(x)
+
+"""
+squeezer(100)(torch.randn(1, 100, 224,224)).shape
+"""    
