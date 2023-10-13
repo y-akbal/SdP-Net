da2f4f13ade7afdee266f956d1689a6cd0f50f45
y-akbal <yildirim.akbal@gmail.com>
2023-10-13 09:57:12 +0300

    "towards_peach_and_atnet"

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ from torch import nn as nn
 from torch.nn import functional as F
 from torch.nn.parameter import Parameter
 import torch.nn.functional as F
+import math
 
 
 from layers import conv_int, conv_mixer, squeezer, first_encoder_layer, encoder_layer
@@ -12,42 +13,61 @@ from layers import conv_int, conv_mixer, squeezer, first_encoder_layer, encoder_
 
 class main_model(nn.Module):
     def __init__(self, 
-                 embedding_dim_conv = 512,
-                 embedding_trans_conv = 256,
-                 n_head = 4,
-                 conv_kernel_size = 5,
-                 conv_mixer_repetation = 5, 
-                 transformer_encoder_repetation = 5,
+                 embedding_dim_conv:int = 512,
+                 image_size:tuple(int, int) = (224,224),
+                 n_head:int = 4,
+                 conv_kernel_size:int = 5,
+                 conv_mixer_repetation:int = 5, 
+                 transformer_encoder_repetation:int = 5,
                  activation = nn.GELU("tanh"),
-                 patch_size = 4,
-                 dropout = 0.2,
-                 squeeze_ratio = 15,
-                 num_register = 2,  
-                 multiplication_factor = 2, 
+                 patch_size:int = 4,
+                 dropout:float = 0.2,
+                 squeeze_ratio:int = 15,
+                 num_register:int = 2,  
+                 multiplication_factor:int = 2, 
                  ):
         super().__init__()
+        self.patch_size = patch_size
+        self.squeeze_ratio = squeeze_ratio
+        self.encoder_embedding_dim = tuple(map(self.encoder_dim, image_size))
+        
+
         self.conv_init = conv_int(embedding_dim= embedding_dim_conv, 
                                   patch_size = patch_size)
-        self.conv_mixer = nn.Sequential(*[
-            conv_mixer(embedding_dim_conv, 
-                       kernel_size= conv_kernel_size)
-                   for i in range(conv_mixer_repetation)])
+        self.conv_mixer = nn.Sequential(*[conv_mixer(embedding_dim_conv, 
+                                        kernel_size= conv_kernel_size)
+                                        for i in range(conv_mixer_repetation)])
         self.squeezer = squeezer(embedding_dim= embedding_dim_conv,
                                  squeeze_ratio= squeeze_ratio,
                                  activation=  activation
                                  )
-        self.encoder = encoder_layer(
-            
+        
+        self.encoder_init = first_encoder_layer(embedding_shape= self.encoder_embedding_dim,
+                                                n_head = n_head,
+                                                num_registers= num_register,
+                                                multiplication_factor= multiplication_factor,
+                                                activation_func= activation,
+                                                dropout=dropout
+                                                )
+        self.encoder_rest= encoder_layer(embedding_shape= self.encoder_embedding_dim,
+                                            n_head = n_head,
+                                            multiplication_factor= multiplication_factor,
+                                            activation_func= activation,
+                                            dropout=dropout,
+
         )
 
+        
+    def encoder_dim(self, n:int)->int:
+        return math.floor(n/(self.patch_size*self.squeeze_ratio))
 
-    def forward(self, x, y):
+    def forward(self, x, y = None):
         x = self.conv_init(x)
         x = self.conv_mixer(x)
         x = self.squeezer(x)
-        return x
+        return self.encoder_init(x, y)
 
-main_model(conv_mixer_repetation=10, squeeze_ratio=3)(torch.randn(1, 3, 224, 224),1).shape
+main_model(conv_mixer_repetation=10, squeeze_ratio= 3, patch_size=8)(x = torch.randn(1, 3, 224, 224)).shape
 
 
 
